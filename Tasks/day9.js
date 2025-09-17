20b9bb482d68c219e8075a8d7821419b25e2da5a
Manikandan235 <manikandanramachandran235@gmail.com>
2025-09-17 10:11:23 +0530

    Add files via upload

---
+++ Tasks/day9.js
@@ -0,0 +1,9 @@
+// Import and Export Moduals
+
+// Definition:
+
+// Export allows yopu to make variables , functions and classes available to other files.
+
+// Import allows you to bring those into another files.
+
+// Used in modular JavaScript (ES6+)
